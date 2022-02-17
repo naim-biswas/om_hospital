f05f64417721b3a92c382e111d3c2358ce2481b8
Naim Biswas <naim.biswas@byslglobal.com>
2022-02-17 19:39:40 +0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+ 'name': 'Hospital Management',
+    'version': '13.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Module for Managing for Hospital',
+    'sequence': '10',
+    'license': 'AGPL-3',
+    'description': 'Hospital Management software',
+    'author': 'Naim Biswas',
+    'website': 'www.bysl.global',
+    'maintainer': 'BYSL Group',
+    'depends': [],
+    'application': True,
+    'data': [
+            'views/patient.xml',
+         ],
+}
